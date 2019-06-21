940ace25f494f53bc0332fdf794e476e07b5042a
yangfan <243832027@qq.com>
2019-06-21 20:50:46 +0800

    实现首页渲染

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,5 +1,8 @@
+from flask import render_template
+from info import redis_store
 from . import index_blu
 
 @index_blu.route('/')
 def index():
-    return 'index'
+
+    return render_template('news/index.html')
